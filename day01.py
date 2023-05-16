034234f691514fc89c8f519b4cec2d6151cab84e
JiaLe <2840245168@qq.com>
2023-05-16 23:56:10 +0800

    V_0.3

---
+++ day01.py
@@ -0,0 +1,10 @@
+"""
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+@Project : pythonProject
+@File : day01.py
+@Author : 18291962907
+@Time : 2023/5/16 21:54
+"""
+print("提交至缓存区")
+print("返回至工作区")
--- 冒牌排序类/demo1.py
+++ 冒牌排序类/demo1.py
@@ -1,12 +1,3 @@
-input1=['h','e','l','l','o',' ','w','o','r','d',' ','j','a','v','a']
-list1=[]
-list2=[]
-list3=[]
-list1.extend(input1[0:5])
-list2.extend(input1[5:11])
-list3.extend(input1[11:15])
-list3.extend(list2)
-list3.extend(list1)
-print(list3)
+print("hello world")
 
 
